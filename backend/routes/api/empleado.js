6ae00341eabcfab838d8a5a37dea9ba4c4725d62
Cesar Mejia <checha365@gmail.com>
2019-02-12 19:44:18 -0600

    Creación del metodo PUT

--- backend/routes/api/empleado.js
+++ backend/routes/api/empleado.js
@@ -15,7 +15,7 @@ var empleadoData = {
     'telefono': '',
 };
 
-//Metodo Get 
+//Metodo GET
 
 router.get('/', function(req, res, next){
     if(!data){
@@ -31,9 +31,9 @@ router.get('/', function(req, res, next){
     } else {
         return res.status(200).json(data);
     }
-}); //Fin del metodo Get
+}); //Fin del metodo GET
 
-//Creación del metodo Post
+//Creación del metodo POST
 router.post('/new', function(req, res, next){
     var _empleadoData = Object.assign({}, empleadoData, req.body);
     if(!data){
@@ -47,7 +47,38 @@ router.post('/new', function(req, res, next){
     }
     return res.status(200).json(_empleadoData);
     });
-});
+});//Fin metodo POST
+
+//Creación metodo PUT
+router.put('/done/:rtn', function(req, res, next){
+    var _empleadoRtn = req.params.RTN;
+    var _empleadoUpdate = req.body;
+    var _empleadoUpdated = null;
+    var nuevo = data.map(
+        function(doc, i){
+            if(doc.rtn == _empleadoRtn){
+                _empleadoUpdated = Object.assign(
+                    {},
+                    doc,
+                    {"done":true},
+                    _empleadoUpdate
+                );
+                return _empleadoUpdated;
+            }
+            return doc;
+        }
+    );
+    data = nuevo;
+    fileModel.write(data, function (err){
+        if(err){
+            console.log(err);
+            return res.status(500).json({'error': 'Error al Guardar los datos'});
+        }
+        return res.status(200).json(_empleadoUpdated);
+    });
+});//Fin metodo PUT
+
+
 
 
 module.exports = router;
